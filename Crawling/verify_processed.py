fdd74c1100bff8f65f7bd2c818c9ac24d9546733
surface03 <ginigini03@gmail.com>
2024-01-22 15:30:53 +0900

    add merged

--- Crawling/verify_processed.py
+++ Crawling/verify_processed.py
@@ -33,7 +33,30 @@ def read_sort_drop(file_path: Path) -> pd.DataFrame:
 def compare_df() -> None:
     d1 = read_sort_drop(vl.MERGED_CRAWLED_JSONL)
     d2 = read_sort_drop(vl.PROCESSED_CRAWLED_JSONL)
-    assert d1.equals(d2), "Merged data and processed data is not equal"
+    # print(d1.iloc[222].to_dict())
+    # print(d2.iloc[222].to_dict())
+    # print(d2)
+    nrows_d1 = d1.shape[0]
+    nrows_d2 = d2.shape[0]
+    if nrows_d1 != nrows_d2:
+        logger.error("두 DataFrame의 행 수가 다릅니다.")
+        sys.exit()
+
+    for i in range(nrows_d1):
+        logger.debug(f"{i}번째 행 비교...")
+        row_d1 = d1.iloc[i]
+        row_d2 = d2.iloc[i]
+
+        # 행 비교
+        if not row_d1.equals(row_d2):
+            logger.error(f"인덱스 {i}에서 차이가 있습니다.")
+            logger.error("d1의 행:")
+            logger.error(row_d1)
+            logger.error("d2의 행:")
+            logger.error(row_d2)
+            logger.error("---")
+            sys.exit()
+    # assert d1.equals(d2), "Merged data and processed data is not equal"
 
 
 def main() -> None:
