0c6bc8cfa2eb85589cf25214b4b3940b47efdfb2
surface03 <ginigini03@gmail.com>
2024-01-23 22:16:22 +0900

    edit appbar

--- Web/worldimpact/src/components/HeaderAppBar.jsx
+++ Web/worldimpact/src/components/HeaderAppBar.jsx
@@ -14,6 +14,10 @@ const HeaderAppBar = ({
 }) => {
   const navigate = useNavigate();
 
+  const gradientStyle = {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)'
+  };
+
   const handleLoginClick = () => {
     navigate("/login");
   };
@@ -28,7 +32,7 @@ const HeaderAppBar = ({
           <img src={Logo} alt="Logo" style={{ cursor: "pointer" }} />
         </Link>
       </div>
-      <AppBar position="static">
+      <AppBar position="static" style={gradientStyle}>
         <Toolbar>
           <TopicTabs
             mainTopics={mainTopics}
